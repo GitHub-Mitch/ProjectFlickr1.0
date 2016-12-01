af9a4bf45e6651a3f2bbaa39f30b4699040dc196
Michel Dufour <michel.dufour.md@gmail.com>
2016-12-01 12:59:55 +0100

    ADD New Activity(FlickrMainActivity), permission Internet, and values xml

---
+++ app/src/main/java/com/mitch/projectflickr10/FlickrMainActivity.java
@@ -0,0 +1,13 @@
+package com.mitch.projectflickr10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlickrMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flickr_main);
+    }
+}
--- app/src/main/java/com/mitch/projectflickr10/MainActivity.java
+++ app/src/main/java/com/mitch/projectflickr10/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mitch.projectflickr10;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btnEnter = (FloatingActionButton) findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FlickrMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
